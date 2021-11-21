1d8fd7c88d9893af7a857a48816f4ea82ca10697
pedroalcantara9568 <alcantara_guitar@hotmail.com>
2021-11-21 09:48:06 -0300

    correção de rest controller

--- src/main/java/com/pedroalcantara/crudreactive/controller/PaymentsController.java
+++ src/main/java/com/pedroalcantara/crudreactive/controller/PaymentsController.java
@@ -3,6 +3,9 @@ package com.pedroalcantara.crudreactive.controller;
 
 import com.pedroalcantara.crudreactive.base.PaymentEntity;
 import com.pedroalcantara.crudreactive.controller.swagger.PaymentDoc;
+import com.pedroalcantara.crudreactive.service.PaymentService;
+import com.pedroalcantara.crudreactive.web.rest.mapper.PaymentMapper;
+import com.pedroalcantara.crudreactive.web.rest.model.PaymentDTO;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -10,23 +13,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-import com.pedroalcantara.crudreactive.service.PaymentService;
-import com.pedroalcantara.crudreactive.web.rest.mapper.PaymentMapper;
-import com.pedroalcantara.crudreactive.web.rest.model.PaymentDTO;
 
+@RestController
 @RequestMapping("/api/payments")
 @AllArgsConstructor
 public class PaymentsController implements PaymentDoc {
 
-    private final PaymentService paymentService;
-
     private PaymentMapper paymentMapper;
+
+    private final PaymentService paymentService;
 
     @PostMapping
     @Override
     public Mono<ResponseEntity<PaymentEntity>> createPayment(
-            @Parameter(schema = @Schema(implementation = PaymentDTO.class))  @RequestBody PaymentDTO paymentDTO
+            @Parameter(schema = @Schema(implementation = PaymentDTO.class)) @RequestBody PaymentDTO paymentDTO
     ) {
         return paymentService.save(paymentMapper.toEntity(paymentDTO)).map(ResponseEntity::ok);
     }
